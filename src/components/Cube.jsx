f851ed39b61eb6b93c09fd791e9aa2914c2ae848
gokhan ozsule <gokhanozsule@gmail.com>
2024-07-22 18:44:04 +0300

    changed the basename in vite.config.js

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -2,7 +2,9 @@ import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 
 export function Cube01(props) {
-  const { nodes, materials } = useGLTF('/models/Cube.glb')
+
+  const modelPath = process.env.PUBLIC_URL + '/models/Cube.glb';
+  const { nodes, materials } = useGLTF(modelPath) 
   return (
     <group {...props} dispose={null}>
       <mesh
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,9 +2,7 @@ import React, { useRef } from 'react'
 import Navbar from './Navbar'
 import { Canvas } from '@react-three/fiber'
 import { Environment, Loader, OrbitControls, Sky, useHelper } from '@react-three/drei'
-import bg from '../images/07.png'
 import { Hero3D } from './Hero3D'
-import { Fog, PointLight, PointLightHelper } from 'three'
 import logo from "../assets/Codesign_Logo_Inv 1.svg"
 
 const Welcome = () => {
@@ -14,7 +12,6 @@ const Welcome = () => {
   return (
     <>
     <div className='w-full h-full rounded-3xl bg-cover bg-center custom-shadow z-10 flex flex-col relative'
-    /* style={{backgroundImage : `url(${bg})`}} */
     >
       <div className="w-full h-full opacity-85 absolute left-0 right-0"></div>
         <Navbar />
@@ -43,12 +40,6 @@ const Welcome = () => {
                 <Hero3D position={[0,-1.5,1]} scale = {1.25} />
             </Canvas>
           </div>
-          {/* <div className='absolute flex flex-col justify-center items-center w-[200px] xl:w-[350px] h-[250px] z-10 top-20 left-10 border border-white rounded-3xl bg-black/25 pointer-events-none text-white'>
-            <h1 className='text-xl -mt-20'>Welcome to a new way of thinking</h1>
-            <h2 className='mb-4 text-lg'>Where the aproach is:</h2>
-            <p>Coding For Designers,</p>
-            <p>Designing for Everyone</p>
-          </div> */}
           <div className="absolute h-full w-full bg-black z-40 flex items-center justify-center landing pointer-events-none">
             <img  src= {logo} className='loading'/>
           </div>
