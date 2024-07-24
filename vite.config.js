d6cfada2c71269afdb7505ce591bf682a88bd46b
gokhan ozsule <gokhanozsule@gmail.com>
2024-07-24 19:50:05 +0300

    github pages fetch error

--- vite.config.js
+++ vite.config.js
@@ -3,29 +3,11 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 
-/* function fixModelPaths() {
-  return {
-    name: 'fix-model-paths',
-    configureServer(server) {
-      server.middlewares.use((req, res, next) => {
-        if (req.url.startsWith('/codesignweb/models/')) {
-          req.url = req.url.replace('/codesignweb/models/', '/models/');
-        }
-        else if (req.url.startsWith('/Works/models/')) {
-          req.url = req.url.replace('/Works/models/', '/models/');
-        }
-        else if (req.url.startsWith('/assets/')) {
-          req.url = req.url.replace('/assets/', '/codesignweb/assets/');
-        }
-        next();
-      });
-    }
-  }
-} */
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()/* , fixModelPaths() */],
+  plugins: [react()],
   base: process.env.VITE_BASE_URL || '/',
   build: {
     rollupOptions: {
