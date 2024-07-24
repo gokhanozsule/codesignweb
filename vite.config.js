4f060facedb49495324c9a40f949b3b45ffaf591
Serkan (E.) Sökmen <serkan@pixelcraftstudios.io>
2024-07-24 22:44:20 +0300

    Cleanup env

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-
-
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/',
+  base: process.env.VITE_BASE_URL || "/",
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-      }
+      },
     },
-  outDir: 'dist'
-  }
-})
+    outDir: "dist",
+  },
+});
